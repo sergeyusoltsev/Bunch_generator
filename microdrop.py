3828bacbf86f2a87a10694d3c273a2c9c237469c
sergeyusoltsev <sergeyusoltsev@yahoo.com>
2022-01-27 17:41:05 +0300

    Major rework
    now there is a parameter file, parsed by microdrop. Microdrop now parses
    commandline arguments

--- microdrop.py
+++ microdrop.py
@@ -1,25 +1,62 @@
-import os
+import os, sys, argparse
 import numpy as np
 
-mol = 'BDP.xyz'
-sol = 'MECN.xyz'
-nat = 31
-nats = 6 
-lr = 6.5
-pss = 6.2
-method = '--gfn 0'
-solvent = 'acetonitrile'
+path = os.path.abspath(__file__)[:-len(os.path.basename(__file__))]
+
+# these are to be commandline parameters
+
+parser = argparse.ArgumentParser(description='Generate molecule with explicit solvation environment')
+parser.add_argument('-mol','--mol', metavar='molecule', type=str, default='molecule.xyz',
+                    help='name of molecule file')
+parser.add_argument('-sol','--sol', metavar='solvent', type=str, default='chcl3',
+                    help='name of solvent, available values are ...')
+parser.add_argument('-method','--method', metavar='--gfn x', type=string, default='--gff',
+                    help='xTB method to use for calculation')
+parser.add_argument('-rt','--rt', metavar='type', type=str, default='normal',
+                    help='Runtype, use normal for full run of solvation free energy')
+parser.add_argument('-nat','--nat', metavar='N', type=int, default=31,
+                    help='number of atoms in the beginning of .xyz to be used as comparator in trimming procedure')
+
+args = parser.parse_args()
+
+cl_solvent = args.sol 
+mol = args.mol
+nat = args.nat
+method = args.method
+runtyp = args.rt
+
+#    flat file with all of the parameters (tab-separated):
+#    solvent,nats,sol,den,mmass,lr,pss
+#    chcl3,5,CHCl3.xyz,1.49,119.38,6.5,6.2
+
+# parse solvent parameters or die
+
+solvent, nats, sol, den, mmass, lr, pss = np.array(str(path) + ".dropparams", delimiter = ',', unpack=True)
+
+notfound = True
+for i, j in enumerate(solvent):
+    if j != cl_solvent:
+        notfound = True
+        continue
+    else:
+        notfound = False
+        solvent = solvent[i]
+        nats = nats[i]
+        sol = sol[i]
+        den = den[i]
+        mmass = mmass[i]
+        lr = lr[i]
+        pss = pss[i]
+sys.exit('solvent not found!') if notfound
+
+# i prefer to leave these hardcoded just for now
+
 mdparams = "$metadyn\n save=10\n kpush=0.02\n alp=1\n$end\n$md\n time=10\n step=1\n temp=298.15\n$end\n"
 mdparams += "$wall\n potential=logfermi\n sphere:auto, all\n autoscale=1.1\n$end"
+
 cleanup = "rm gfnff_*;rm xtbrestart"
 
-#data for 1 Mole of liquid at 1 atm to 1M solution conversion
-den = 0.786 # CHCl3 
-mmass = 41.05 # CHCl3
-
-selfc = den * 1000 / mmass
-expls = 29 
-lstdfac = 1.987204 * 298.15 * np.log(selfc/expls) /1000
+### definition of the external command lists for subroutines
 
 generate = [cleanup, 
 "python bunch_generator.py --mol %s --sol %s --cr 2.5 --lr 999 --mr 25 -pss %f" %(mol, sol, pss),
@@ -56,25 +93,46 @@ cleanup,
 "xtb STG2_S.xyz --bhess  %s > STG2_S_THERMO.out" %(method),
 "cat STG2_S_THERMO.out | grep \'TOTAL FREE\' | tr -s \' \' | cut -d\' \' -f 6 >> datapoints.txt",
 cleanup,
-"xtb STG2_%s.xyz --bhess  %s --gbsa %s > STG2_%s_THERMO.out" %(solvent, method, solvent, solvent),
+"xtb STG2_%s.xyz --bhess  %s --gbsa %s reference > STG2_%s_THERMO.out" %(solvent, method, solvent, solvent),
 "cat STG2_%s_THERMO.out | grep \'TOTAL FREE\' | tr -s \' \' | cut -d\' \' -f 6 >> datapoints.txt" %(solvent),
 cleanup,
-"xtb STG2_%s_S.xyz --bhess  %s --gbsa %s > STG2_%s_S_THERMO.out" %(solvent, method, solvent, solvent),
+"xtb STG2_%s_S.xyz --bhess  %s --gbsa %s reference > STG2_%s_S_THERMO.out" %(solvent, method, solvent, solvent),
 "cat STG2_%s_S_THERMO.out | grep \'TOTAL FREE\' | tr -s \' \' | cut -d\' \' -f 6 >> datapoints.txt" %(solvent),
 cleanup]
 
-print('--- GENERATION STEP ---')
-for i in generate:
-    os.system(i)
-print('--- OPTIMIZATION STEP ---')
-for i in optimize:
-    os.system(i)
-print('--- THERMOCHEMISTRY STEP ---')
-for i in thermochem:
-    os.system(i)
+def generate(generate)
+    print('--- GENERATION STEP ---')
+    for i in generate:
+        os.system(i)
 
-path = os.path.abspath(__file__)[:-len(os.path.basename(__file__))]
-gees = np.array(np.genfromtxt(str(path) + 'datapoints.txt'))
-deltagee = (gees[1] - gees[0] - gees[2] + (gees[3] - gees[1]) - (gees[4] - gees[2])) * 627.5
-deltageecorr = ( deltagee - 1.89 - lstdfac ) * 4.184
-print('solvation free energy equals ' + str(deltageecorr) + ' kJ/mol')
+def omtimize(optimize)
+    print('--- OPTIMIZATION STEP ---')
+    for i in optimize:
+        os.system(i)
+
+def geecalc(path, thermochem)
+    print('--- THERMOCHEMISTRY STEP ---')
+    for i in thermochem:
+        os.system(i)
+        gees = np.array(np.genfromtxt(str(path) + 'datapoints.txt'))
+    return gees
+
+def dgeecalc(path, gees, den, mmass):
+    # amount of explicit solvent molecules, unoptimal way to get it is to parse file generated upon 
+    # bunch_generator.py run each time.
+    expls = np.genfromtxt(str(path) + '.samnt')
+    # equilibrium concentration of 1 mole of solution at 1 atm
+    selfc = den * 1000 / mmass
+    # term to correct to 1 mol/L solution standard state
+    lstdfac = 1.987204 * 298.15 * np.log(selfc/expls) /1000
+    # calculate to thermodynamic cycle as in reference [], 627.5 is for conversion from eH to kcal/mol
+    deltagee = (gees[1] - gees[0] - gees[2] + (gees[3] - gees[1]) - (gees[4] - gees[2])) * 627.5
+    # adding all correction factors and converting to kJ/mol
+    deltageecorr = ( deltagee - 1.89 - lstdfac ) * 4.184
+    print('solvation free energy equals ' + str(deltageecorr) + ' kJ/mol')
+
+def normal(path, generate, optimize, thermochem, den, mmass):
+    generate(generate)
+    optimize(optimize)
+    gees = thermochem(path, thermochem)
+    dgeecalc(path, gees, den, mmass)
